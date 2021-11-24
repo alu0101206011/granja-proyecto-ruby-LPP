3a97944818e948ef2cef9082f792cec92280f00b
alu0101206011 <alu0101206011@ull.edu.es>
2021-11-24 16:21:48 +0000

    Atributo de la clase Funcion de la reproducción de los animales creado

--- lib/farm/funcion.rb
+++ lib/farm/funcion.rb
@@ -1,9 +1,10 @@
 module Farm
   class Funcion
-    attr_reader :vida, :cuidados
-    def initialize(vida, cuidados)
+    attr_reader :vida, :cuidados, :reproduccion
+    def initialize(vida, cuidados, reproduccion)
       @vida = vida
       @cuidados = cuidados
+      @reproduccion = reproduccion
     end
   end
 end
