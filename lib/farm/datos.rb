1c8ce5f710323316abbd7ae16caed3bda6d6c83d
alu0101206011 <alu0101206011@ull.edu.es>
2021-11-24 17:45:43 +0000

    Atributo descripcion creado

--- lib/farm/datos.rb
+++ lib/farm/datos.rb
@@ -1,10 +1,11 @@
 module Farm
   class Datos
-    attr_reader :id, :nombre, :tipo
-    def initialize(id, nombre, tipo)
+    attr_reader :id, :nombre, :tipo, :descripcion
+    def initialize(id, nombre, tipo, descripcion)
       @id = id
       @nombre = nombre
       @tipo = tipo
+      @descripcion = descripcion
     end
   end
 end
