3f5f088a14bc0c7d52d79303d0897be371a02212
alu0101206011 <alu0101206011@ull.edu.es>
2021-11-24 17:38:10 +0000

    Atributo tipo creado

--- lib/farm/datos.rb
+++ lib/farm/datos.rb
@@ -1,9 +1,10 @@
 module Farm
   class Datos
-    attr_reader :id, :nombre
-    def initialize(id, nombre)
+    attr_reader :id, :nombre, :tipo
+    def initialize(id, nombre, tipo)
       @id = id
       @nombre = nombre
+      @tipo = tipo
     end
   end
 end
