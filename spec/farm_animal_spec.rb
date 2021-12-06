4fde2e93e79b3b85d7b7a7ace787b7aa2ceb2ad8
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-06 19:09:54 +0000

    Expectativa de un atributo para representar el peso del animal en gramos creada

--- spec/farm_animal_spec.rb
+++ spec/farm_animal_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe Farm do
   context "Representación de un Animal - Granja::Animal\n" do
     describe Farm::Animal do
       before :each do
-        @animal1 = Farm::Animal.new(1, 400, "M")
+        @animal1 = Farm::Animal.new(1, 400, "M", 10000)
       end  
+      
       it "Tiene una clase para representar animales" do
         expect(@animal1).not_to be_nil
         expect(@animal1).to be_kind_of(Farm::Animal)
@@ -22,6 +23,10 @@ RSpec.describe Farm do
 
       it "Tiene un atributo para representar el sexo del animal" do
         expect(@animal1.sexo).to eq("M")
+      end
+
+      it "Tiene un atributo para representar el peso del animal en gramos" do
+        expect(@animal1.peso).to eq(10000)
       end
     end
   end
