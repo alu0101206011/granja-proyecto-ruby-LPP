94c1a983896ddf686f77c62781905e54c477ffc7
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-06 20:12:15 +0000

    Creación del initialize, con los atributos que son del padre y el atributo raza

--- lib/farm/ganado.rb
+++ lib/farm/ganado.rb
@@ -4,5 +4,14 @@ module Farm
   # Clase Ganado
   # Contiene información sobre el ganado. Es una clase heredada de Animal.
   class Ganado < Animal
+    attr_reader :raza
+
+    # Constructor
+    # Usando super podemos usar los atributos del padre
+    # @param [raza] integer Contiene el identificador del animal
+    def initialize(id, edad, sexo, peso, raza)
+      super(id, edad, sexo, peso)
+      @raza = raza
+    end
   end
 end
