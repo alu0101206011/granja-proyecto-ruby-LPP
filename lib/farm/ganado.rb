22b7afe790781452b683fa9ca58b578818b0515f
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-06 19:55:18 +0000

    Clase Ganado Creada

---
+++ lib/farm/ganado.rb
@@ -0,0 +1,8 @@
+# @author Anabel Díaz Labrador
+# Gema Farm
+module Farm
+  # Clase Ganado
+  # Contiene información sobre el ganado. Es una clase heredada de Animal.
+  class Ganado < Animal
+  end
+end
