23ace5fd480db6668fb89bb062c553c1204f0012
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-06 19:04:22 +0000

    Expectativa de un atributo para representar el sexo de un animal creada

--- spec/farm_animal_spec.rb
+++ spec/farm_animal_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Farm do
   context "Representación de un Animal - Granja::Animal\n" do
     describe Farm::Animal do
       before :each do
-        @animal1 = Farm::Animal.new(1, 400)
+        @animal1 = Farm::Animal.new(1, 400, "M")
       end  
       it "Tiene una clase para representar animales" do
         expect(@animal1).not_to be_nil
@@ -18,6 +18,10 @@ RSpec.describe Farm do
       
       it "Tiene un atributo para representar la edad del animal en días" do
         expect(@animal1.edad).to eq(400)
+      end
+
+      it "Tiene un atributo para representar el sexo del animal" do
+        expect(@animal1.sexo).to eq("M")
       end
     end
   end
