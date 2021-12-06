09cf83526364f6343bd40d65b0e1be77138e45f9
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-06 20:06:26 +0000

    Expectativa de un atributo para representar la raza del ganado

--- spec/farm_ganado_spec.rb
+++ spec/farm_ganado_spec.rb
@@ -5,9 +5,20 @@ RSpec.describe Farm do
   context "Representación de un ganado - Farm::Ganado" do
     describe Farm::Ganado do
       before :each do
-        @ganado1 = Farm::Ganado.new()
+        @ganado1 = Farm::Ganado.new(1, 400, "M", 15000, "caprino")
       end  
       
+      context "Atributos de la clase Ganado" do
+        it "Tiene una clase para representar ganado" do
+          expect(@ganado1).to be_kind_of(Farm::Ganado)
+        end
+
+        it "Tiene un atributo para representar la raza (bovino, porcino, ovino o caprino)" do
+          expect(@ganado1.raza).to eq("caprino")
+        end
+
+
+      end
         
     end
   end
