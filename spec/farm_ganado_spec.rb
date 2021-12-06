5feeb8fd28dd8607b326911cf29c5965a7ee045b
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-06 20:48:40 +0000

    Expectativa de que la instancia de la clase Ganado sea un objeto creada y funcional

--- spec/farm_ganado_spec.rb
+++ spec/farm_ganado_spec.rb
@@ -38,6 +38,10 @@ RSpec.describe Farm do
         it "Se espera que una instancia de la clase Ganado sea un Animal" do
           expect(@ganado1).to be_kind_of(Farm::Animal)
         end
+
+        it "Se espera que una instancia de la clase Ganado sea un objeto (Object)" do
+          expect(@ganado1).to be_kind_of(Object)
+        end
       end
     end
   end
