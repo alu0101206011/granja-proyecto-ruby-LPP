fab3f04a9073af8f60a16f8a800b1edcb339876e
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-06 19:00:02 +0000

    Atributo edad creado

--- lib/farm/animal.rb
+++ lib/farm/animal.rb
@@ -4,12 +4,14 @@ module Farm
   # Clase Animal
   # Contiene información sobre animales
   class Animal
-    attr_reader :id
+    attr_reader :id, :edad
 
     # Constructor
     # @param [id] integer Contiene el identificador del animal
-    def initialize(id)
+    # @param [edad] integer Contiene la edad del animal en días
+    def initialize(id, edad)
       @id = id
+      @edad = edad
     end
   end
 end
