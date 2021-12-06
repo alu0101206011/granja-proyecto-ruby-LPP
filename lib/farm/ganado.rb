49957e9649103bf1f1694bf5145ac51ad5bd0e9d
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-06 20:27:17 +0000

    Atributo alimentación creado

--- lib/farm/ganado.rb
+++ lib/farm/ganado.rb
@@ -4,16 +4,18 @@ module Farm
   # Clase Ganado
   # Contiene información sobre el ganado. Es una clase heredada de Animal.
   class Ganado < Animal
-    attr_reader :raza, :aprovechamiento
+    attr_reader :raza, :aprovechamiento, :alimentacion
 
     # Constructor
     # Usando super podemos usar los atributos del padre
     # @param [raza] integer Contiene la representación de la raza del animal
-    # @param [aprovechamiento] string Contiene la representación del tipo de aprovechamiento 
-    def initialize(id, edad, sexo, peso, raza, aprovechamiento)
+    # @param [aprovechamiento] string Contiene la representación del tipo de aprovechamiento (carne, piel, leche)
+    # @param [alimentacion] string Contiene la representación del tipo de alimentación (herbívoro u omnívoro)
+    def initialize(id, edad, sexo, peso, raza, aprovechamiento, alimentacion)
       super(id, edad, sexo, peso)
       @raza = raza
       @aprovechamiento = aprovechamiento
+      @alimentacion = alimentacion
     end
   end
 end
