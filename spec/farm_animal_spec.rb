5571a83278e6dd7e758b3db16bef9b41114d045b
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-06 18:49:41 +0000

    Expectativa de un atributo para identificar un animal creada

--- spec/farm_animal_spec.rb
+++ spec/farm_animal_spec.rb
@@ -5,11 +5,17 @@ RSpec.describe Farm do
   context "Representación de un Animal - Granja::Animal\n" do
     describe Farm::Animal do
       before :each do
-        @animal1 = Farm::Animal.new()
+        @animal1 = Farm::Animal.new(1)
       end  
       it "Tiene una clase para representar animales" do
         expect(@animal1).not_to be_nil
+        expect(@animal1).to be_kind_of(Farm::Animal)
       end
+
+      it "Tiene un atributo para identificar al animal" do
+        expect(@animal1.id).to eq(1)
+      end
+
     end
   end
 end
