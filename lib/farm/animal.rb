7c0fa89cd209abbad3ea844f00756df4df8fc9a5
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-06 21:09:37 +0000

    Atributo de clase animal_count creado

--- lib/farm/animal.rb
+++ lib/farm/animal.rb
@@ -6,6 +6,9 @@ module Farm
   class Animal
     attr_reader :id, :edad, :sexo, :peso
 
+    # Atributo de clase para contar el número de  de objetos que se instancia de la clase.
+    @@animal_count = 0
+
     # Constructor
     # @param [id] integer Contiene el identificador del animal
     # @param [edad] integer Contiene la edad del animal en días
@@ -16,12 +19,18 @@ module Farm
       @edad = edad
       @sexo = sexo
       @peso = peso
+      @@animal_count += 1
     end
-
 
     # Método que retorna un string con la información perfectamente formateada
     def to_s
       return "Animal con id: #{id}\nEdad (días): #{edad}\nSexo: #{sexo}\nPeso (gramos): #{peso}"
     end
+
+    # Método de clase que retorna el valor del atributo de clase animal_count
+    def self.animal_count
+      @@animal_count
+    end
+
   end
 end
