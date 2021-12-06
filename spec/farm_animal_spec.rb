1a79a19e5e463acc5fef5ff18cc769ecb0589c4c
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-06 18:34:13 +0000

    Expectativa de la creación de una clase para representar animales

--- spec/farm_animal_spec.rb
+++ spec/farm_animal_spec.rb
@@ -1,1 +1,15 @@
+require 'spec_helper'
+require './lib/farm/animal'
 
+RSpec.describe Farm do
+  context "Representación de un Animal - Granja::Animal\n" do
+    describe Farm::Animal do
+      before :each do
+        @animal1 = Farm::Animal.new()
+      end  
+      it "Tiene una clase para representar animales" do
+        expect(@animal1).not_to be_nil
+      end
+    end
+  end
+end
