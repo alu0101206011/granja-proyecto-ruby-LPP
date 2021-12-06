c19cae04417ea5c283778a7003f3374c75c207c3
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-06 19:45:08 +0000

    Expectativa de la creación de una clase para representar ganado creada

---
+++ spec/farm_ganado_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require './lib/farm/ganado'
+
+RSpec.describe Farm do
+  context "Representación de un ganado - Farm::Ganado" do
+    describe Farm::Ganado do
+      before :each do
+        @ganado1 = Farm::Ganado.new()
+      end  
+      
+        
+    end
+  end
+end
