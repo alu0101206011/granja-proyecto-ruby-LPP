fb42a03ac1de53070d82e8a4ff3f3ee77db0a8e4
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-06 19:05:47 +0000

    Atributo sexo creado

--- lib/farm/animal.rb
+++ lib/farm/animal.rb
@@ -4,14 +4,16 @@ module Farm
   # Clase Animal
   # Contiene información sobre animales
   class Animal
-    attr_reader :id, :edad
+    attr_reader :id, :edad, :sexo
 
     # Constructor
     # @param [id] integer Contiene el identificador del animal
     # @param [edad] integer Contiene la edad del animal en días
-    def initialize(id, edad)
+    # @param [sexo] string Contiene el sexo del animal
+    def initialize(id, edad, sexo)
       @id = id
       @edad = edad
+      @sexo = sexo
     end
   end
 end
