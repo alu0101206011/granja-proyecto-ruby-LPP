ac404e214451c61f3b9ffc8058d540b9844feca5
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-07 20:43:16 +0000

    Expectativas sobre la herencia del módulo Funcion realizadas y funcionales

--- spec/farm_funcion_spec.rb
+++ spec/farm_funcion_spec.rb
@@ -21,6 +21,23 @@ RSpec.describe Farm do
           expect(Farm::Funcion::reproduccion("Vivíparo")).to eq("Vivíparo")
         end
       end
+      context "Herencia del módulo Funcion" do
+        it "Se espera que sea un objeto de la clase Module" do
+          expect(Farm::Funcion).to be_instance_of(Module)
+        end
+        it "Se espera que sea un objeto (Object)" do
+          expect(Farm::Funcion).to be_kind_of(Object)
+        end
+        it "Se espera que sea un objeto básico (BasicObject)" do
+          expect(Farm::Funcion).to be_kind_of(BasicObject)
+        end
+        it "No se espera que sea una instancia de la clase que representa una cadena (String)" do
+          expect(Farm::Funcion).not_to be_kind_of(String)
+        end
+        it "No se espera que sea una instancia de la clase que representa un número (Numeric)" do
+          expect(Farm::Funcion).not_to be_kind_of(Numeric)
+        end        
+      end
     end
   end
 end
