c07ff9bdf331244ab2fa917cbe2e4f770f46c8fc
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-07 18:04:59 +0000

    Modulo función creado

--- lib/farm/funcion.rb
+++ lib/farm/funcion.rb
@@ -1,24 +1,9 @@
 # @author Anabel Díaz Labrador
 # Gema Farm
 module Farm
-  # Clase Funcion
+  # Modulo Funcion
   # Contiene las funcionalidades de la granja
-  class Funcion
-    attr_reader :vida, :cuidados, :reproduccion
-
-    # Constructor
-    # @param [vida] string Contiene las condiciones de vida
-    # @param [cuidados] string Contiene los tipos de cuidados
-    # @param [reproduccion] string Contiene la reproducción 
-    def initialize(vida, cuidados, reproduccion)
-      @vida = vida
-      @cuidados = cuidados
-      @reproduccion = reproduccion
-    end
-
-    # Método que pasa el objeto Funcion a string
-    def to_s 
-      return "Condiciones de vida: #{@vida}\nTipos de cuidados: #{@cuidados}\nReproducción: #{@reproduccion}"
-    end    
+  module Funcion
+    
   end
 end
