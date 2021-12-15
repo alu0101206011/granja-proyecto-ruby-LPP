cfc5af11a83a69e87e4cb21397e8f059e3f6b48b
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-15 21:33:30 +0000

    Atributo para el numero de animales creado

--- lib/farm/ganadera.rb
+++ lib/farm/ganadera.rb
@@ -8,11 +8,14 @@ module Farm
 
     # Constructor
     # Usando super podemos usar los atributos del padre
-    # @param [string] tipo_ganado Contiene el tipo de ganado que se encuentra en la granja
-    def initialize(id, nombre, tipo, descripcion, tipo_ganado, destino) 
+    # @param [string] tipo_ganado Contiene el tipo de ganado que se encuentra en la granja (bovino, ovino, caprino o porcino)
+    # @param [string] destino Contiene el destino de los animales (leche, sacrificio)
+    # @param [integer] numero_animales Contiene el numero de animales de la granja
+    def initialize(id, nombre, tipo, descripcion, tipo_ganado, destino, numero_animales) 
       super(id, nombre, tipo, descripcion)
       @tipo_ganado = tipo_ganado
       @destino = destino
+      @numero_animales = numero_animales
     end
   end
 end
