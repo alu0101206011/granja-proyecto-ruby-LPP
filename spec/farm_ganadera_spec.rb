604863f6d8b2a10111c212f852087a7933b6e13a
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-15 20:53:01 +0000

    Expectativa de que la clase Ganadera almacene los datos de una granja realizada y funcional

--- spec/farm_ganadera_spec.rb
+++ spec/farm_ganadera_spec.rb
@@ -7,6 +7,13 @@ RSpec.describe Farm do
       before :each do 
         @granja_ganadera = Farm::Ganadera.new(1, "Granja Labrador", "ganadera", "Granja que tiene gallinas y vacas situada al norte de Tenerife.")
       end
+
+      context "Atributos de la clase Ganadera" do
+        it "Tiene una clase para almacenar los datos de la granja" do
+          expect(@granja_ganadera.instance_of?Farm::Ganadera).to eq(true)
+        end
+      end
+
       context "Herencia de la clase Ganadera" do
         it "Se espera que una instancia de la clase Ganadera sea una granja Ganadera" do
           expect(@granja_ganadera).to be_instance_of(Farm::Ganadera)
