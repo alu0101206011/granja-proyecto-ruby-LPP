b154df2eb09eeb25f7a0455a9cf6aa14df3aa006
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-15 20:11:58 +0000

    Clase Ganadera creada

---
+++ lib/farm/ganadera.rb
@@ -0,0 +1,9 @@
+# @author Anabel Díaz Labrador
+# Gema Farm
+module Farm
+  # Clase Ganadera
+  # Clase para representar Ganadera
+  class Ganadera < Datos
+
+  end
+end
--- spec/farm_ganadera_spec.rb
+++ spec/farm_ganadera_spec.rb
@@ -3,13 +3,14 @@ require './lib/farm/ganadera'
 
 RSpec.describe Farm do
   context "Representación de una granja ganadera - Farm::Ganadera" do
-    describe Farm::Ganado do
+    describe Farm::Ganadera do
       before :each do 
-        @granja_ganadera = Farm::Ganadera.new()
+        @granja_ganadera = Farm::Ganadera.new(1, "Granja Labrador", "ganadera", "Granja que tiene gallinas y vacas situada al norte de Tenerife.")
       end
-      context "Herencia de la clase Ganadera"
-      it "Se espera que una instancia de la clase Ganadera sea una granja Ganadera" do
-        expect(@granja_ganadera).to be_instance_of(Farm::Ganadera)
+      context "Herencia de la clase Ganadera" do
+        it "Se espera que una instancia de la clase Ganadera sea una granja Ganadera" do
+          expect(@granja_ganadera).to be_instance_of(Farm::Ganadera)
+        end
       end
     end
   end
