df79d8c098f90620cf2bf92aa9b8fb34dc31d799
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-15 21:25:01 +0000

    Atributo destino creado

--- lib/farm/ganadera.rb
+++ lib/farm/ganadera.rb
@@ -4,14 +4,15 @@ module Farm
   # Clase Ganadera
   # Clase para representar Ganadera
   class Ganadera < Datos
-    attr_reader :tipo_ganado
+    attr_reader :tipo_ganado, :destino
 
     # Constructor
     # Usando super podemos usar los atributos del padre
     # @param [string] tipo_ganado Contiene el tipo de ganado que se encuentra en la granja
-    def initialize(id, nombre, tipo, descripcion, tipo_ganado) 
+    def initialize(id, nombre, tipo, descripcion, tipo_ganado, destino) 
       super(id, nombre, tipo, descripcion)
       @tipo_ganado = tipo_ganado
+      @destino = destino
     end
   end
 end
