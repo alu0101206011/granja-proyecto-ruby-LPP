9d3b1638df26d6c5d593d9ecd2281d7e2630bf7e
alu0101206011 <alu0101206011@ull.edu.es>
2021-12-16 16:17:22 +0000

    Expectativa de establecer cuidados a los animales

--- spec/farm_funcion_spec.rb
+++ spec/farm_funcion_spec.rb
@@ -14,14 +14,32 @@ RSpec.describe Farm do
           expect(Farm::Funcion::CONDICIONES_VIDA[0]).to eq("campo abierto")
         end
 
+        it "Existe una constante para representar el sistema de gestión por jaulas" do
+          expect(Farm::Funcion::JAULA).to eq(:jaula)
+        end
+
+        it "Existe una constante para representar el sistema de gestión por campo abierto" do
+          expect(Farm::Funcion::CAMPO_ABIERTO).to eq(:campo_abierto)
+        end
+
         it "Existe un procedimiento para establecer los cuidados de los animales" do
-          expect(Farm::Funcion::cuidados("Bueno")).to eq("Bueno")
+          @ganado1 = Farm::Ganado.new(1, 400, "M", 15000, "caprino", "leche", "omnívoro")
+          @ganado2 = Farm::Ganado.new(2, 235, "M", 40000, "bovino", "leche", "herbívoro")
+          @grupo1 = [@ganado1, @ganado2]
+
+          @ganadoCuidado1 = Farm::Ganado.new(1, 410, "M", 15000, "caprino", "leche", "omnívoro")
+          @ganadoCuidado2 = Farm::Ganado.new(2, 245, "M", 40000, "bovino", "leche", "herbívoro")
+          @grupoCuidado1 = [@ganadoCuidado1, @ganadoCuidado2]
+
+          expect(Farm::Funcion::cuidados(10,@grupo1)).to eq(@grupoCuidado1)
+
         end
 
         it "Existe un procedimiento para establecer la reproducción de los animales" do
           expect(Farm::Funcion::reproduccion("Vivíparo")).to eq("Vivíparo")
         end
       end
+
       context "Herencia del módulo Funcion" do
         it "Se espera que sea un objeto de la clase Module" do
           expect(Farm::Funcion).to be_instance_of(Module)
@@ -38,16 +56,6 @@ RSpec.describe Farm do
         it "No se espera que sea una instancia de la clase que representa un número (Numeric)" do
           expect(Farm::Funcion).not_to be_kind_of(Numeric)
         end        
-      end
-
-      context "Sistemas de gestion de animales de granja" do
-        it "Existe una constante para representar el sistema de gestión por jaulas" do
-          expect(Farm::Funcion::JAULA).to eq(:jaula)
-        end
-
-        it "Existe una constante para representar el sistema de gestión por campo abierto" do
-          expect(Farm::Funcion::CAMPO_ABIERTO).to eq(:campo_abierto)
-        end
       end
     end
   end
