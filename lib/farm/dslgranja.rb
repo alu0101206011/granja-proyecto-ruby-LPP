48146941595e3cc3d037ad3532da17437a3ee4a6
alu0101206011 <alu0101206011@ull.edu.es>
2022-01-18 14:34:31 +0000

    Método para los datos creado

--- lib/farm/dslgranja.rb
+++ lib/farm/dslgranja.rb
@@ -4,6 +4,36 @@ module Farm
   # Clase DSLGranja
   # Clase para representar granjas con DSL
   class DSLGranja
-   
+    # Constructor
+    # @param [integer] id Identificador de la granja
+    # @param [Farm::DSLGranja] block bloque dado para meter la información en la clase
+    def initialize(id, &block)
+      @id = id.to_s
+      @datos = []
+      @ejemplar = []
+
+      if block_given?  
+        if block.arity == 1
+          yield self
+        else
+        instance_eval(&block) 
+        end
+      end
+    end
+
+    def dato (texto, descripcion = {}, tipo = {})
+      dato = texto
+      dato << " (#{descripcion[:descripcion]})" if descripcion[:descripcion]
+      dato << " (#{tipo[:tipo]})" if tipo[:tipo]
+      @datos << dato      
+    end
+
+    def to_s
+      output = @id
+      output << "\n#{'=' * @id.size}\n\n"
+      output << "Datos: #{@datos.join(', ')}\n\n"
+  
+      output
+    end
   end
 end
--- spec/farm_DSLGranja_spec.rb
+++ spec/farm_DSLGranja_spec.rb
@@ -6,7 +6,7 @@ require './lib/farm/dslgranja'
 RSpec.describe Farm do
   describe Farm::DSLGranja do 
     before :each do
-      @granja_1 = Farm::DSLGranja.new() do
+      @granja_1 = Farm::DSLGranja.new(12345678) do
 
       end
     end
@@ -17,11 +17,11 @@ RSpec.describe Farm do
       
       it "Tiene un método para los datos"  do
         @granja1 = Farm::DSLGranja.new(12345678) do
-          datos "Pollos muertos",
+          dato "Pollos muertos",
           :descripcion => "PyME - Pequeña y mediana empresa",
           :tipo => :pollos
         end
-        expect(@granja1)
+        expect(@granja1.to_s).to eq("12345678\n========\n\nDatos: Pollos muertos (PyME - Pequeña y mediana empresa)\n\n")
       end
     end
 
