ff77e135931a38e6fe381ae56e3e9660130ad93f
alu0101206011 <alu0101206011@ull.edu.es>
2022-01-18 15:36:44 +0000

    Expectativa de mostrar la información correctamente formateada creada y funcional

--- lib/farm/dslgranja.rb
+++ lib/farm/dslgranja.rb
@@ -10,7 +10,7 @@ module Farm
     def initialize(id, &block)
       @id = id.to_s
       @datos = []
-      @ejemplar = []
+      @ejemplares = []
 
       if block_given?  
         if block.arity == 1
@@ -28,11 +28,24 @@ module Farm
       @datos << dato      
     end
 
+    def ejemplar (texto, edad = {}, peso = {}, precio_compra = {}, precio_venta = {})
+      ejemplar = texto
+      ejemplar << " (#{edad[:edad]})" if edad[:edad]
+      ejemplar << " (#{peso[:peso]})" if peso[:peso]
+      ejemplar << " (#{precio_compra[:precio_compra]})" if precio_compra[:precio_compra]
+      ejemplar << " (#{precio_venta[:precio_venta]})" if precio_venta[:precio_venta]
+      @ejemplares << ejemplar      
+    end
+
     def to_s
       output = @id
       output << "\n#{'=' * @id.size}\n\n"
-      output << "Datos: #{@datos.join(', ')}\n\n"
-  
+      if (!@datos.empty?) 
+        output << "Datos: #{@datos.join(', ')}\n\n"
+      end
+      if (!@ejemplares.empty?) 
+        output << "Ejemplares: #{@ejemplares.join(', ')}\n\n"
+      end
       output
     end
   end
--- spec/farm_DSLGranja_spec.rb
+++ spec/farm_DSLGranja_spec.rb
@@ -5,13 +5,9 @@ require './lib/farm/dslgranja'
 
 RSpec.describe Farm do
   describe Farm::DSLGranja do 
-    before :each do
-      @granja_1 = Farm::DSLGranja.new(12345678) do
-
-      end
-    end
     context "Atributos de la clase DSLGranja" do
       it "Tiene una clase para almacenar los datos de la granja." do
+        @granja_1 = Farm::DSLGranja.new(12345678)
         expect(@granja_1).to be_instance_of(Farm::DSLGranja)
       end
       
@@ -32,7 +28,26 @@ RSpec.describe Farm do
           :precio_compra => 4.25,
           :precio_venta => 4.75
         end
-        expect(@granja1.to_s).to eq("12345678\n========\n\nDatos: Pollos muertos (PyME - Pequeña y mediana empresa)\n\n")
+        expect(@granja1.to_s).to eq("12345678\n========\n\nEjemplares: 12345678-00000001 (365)\n\n")
+      end
+
+      it "Se obtiene una cadena con la información de la granja correctamente formateada" do
+        @granja1 = Farm::DSLGranja.new(12345678) do
+          dato "Pollos muertos",
+          :descripcion => "PyME - Pequeña y mediana empresa",
+          :tipo => :pollos
+          ejemplar "12345678-00000001",
+          :edad => 365,
+          :peso => 700.2,
+          :precio_compra => 4.25,
+          :precio_venta => 4.75
+          ejemplar "12345678-00000002",
+          :edad => 465,
+          :peso => 1200.2,
+          :precio_compra => 1.25,
+          :precio_venta => 2.75
+          end
+          expect(@granja1.to_s).to eq("12345678\n========\n\nDatos: Pollos muertos (PyME - Pequeña y mediana empresa)\n\nEjemplares: 12345678-00000001 (365), 12345678-00000002 (465)\n\n")
       end
     end
 
